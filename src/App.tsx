5f0f401d119121f49d67a579f637d98f5fd88585
Dylan920424 <dylan920424@gmail.com>
2024-07-22 12:36:37 -0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+import "@fontsource/poppins/700.css"
+import "@fontsource/poppins/600.css"
+
+function App() {
+
+  return (
+    <>
+      <div className="header">
+        <h1>Damous</h1>
+      </div>
+      <div className="hero-text">
+        <div className="words">
+          <span>Work</span>
+          <span>Code</span>
+          <span>Train</span>
+        </div>
+        <p className='text1'>Like You Have</p>
+        <p className='text2'>The Data</p>
+      </div>
+    </>
+  )
+}
+
+export default App
